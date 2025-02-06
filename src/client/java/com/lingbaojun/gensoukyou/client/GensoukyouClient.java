95da2908c13578dd300a12f8e2cc802f5179ad1e
lingbaojun233 <yang403033@126.com>
2025-02-07 00:11:16 +0800

    init

---
+++ src/client/java/com/lingbaojun/gensoukyou/client/GensoukyouClient.java
@@ -0,0 +1,10 @@
+package com.lingbaojun.gensoukyou.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GensoukyouClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/lingbaojun/gensoukyou/Gensoukyou.java
@@ -0,0 +1,10 @@
+package com.lingbaojun.gensoukyou;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Gensoukyou implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
